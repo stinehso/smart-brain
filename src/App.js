180a5d6dce840d60c4845e28bf972b572df6c5ae
Stine <stinehso@gmail.com>
2020-04-30 16:16:57 +0200

    update package.json

--- src/App.js
+++ src/App.js
@@ -61,6 +61,11 @@ class App extends Component {
         }})
     }
 
+    componentDidMount() {
+        fetch('https://desolate-plateau-02589.herokuapp.com/')
+            .then(response => response.json())
+            .then(console.log)
+    }
 
 	calculateFaceLocation = (data) => {
 		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
